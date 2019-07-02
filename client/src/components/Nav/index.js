76b12fbeb766f3dbfaf684f2f2e5377bb3379852
HermesCat <jppettis@comcast.net>
2019-07-02 18:18:20 -0400

    bootstrap cdn

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,32 @@
 import React from "react";
+import "./nav.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+
+    <>
+      <header>
+        <div className="row">
+          <div cassName="col-6">
+            <h1><span className="hot">Quick</span>Aggregate</h1>
+          </div>
+          <div cassName="col-6">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+              <a className="navbar-brand" href="/">
+                HOME
+    </a>
+              <a className="navbar-brand" href="/">
+                ABOUT
+    </a>
+              <a className="navbar-brand" href="/">
+                CONTACT
+    </a>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+
   );
 }
 

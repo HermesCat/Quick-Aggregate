4a616d57d73a8f46c50ee298760c623e87f56565
HermesCat <jppettis@comcast.net>
2019-07-06 14:13:40 -0400

    got the desktop working, need to fix a few things on mobile

--- client/src/components/Search_Box/index.js
+++ client/src/components/Search_Box/index.js
@@ -8,7 +8,7 @@ import "./style.css";
 function Search_Box() {
     return (
         <div id="accordion">
-            <div class="card">
+            <div class="card" id="accordion1">
                 <div class="card-header" id="headingOne">
                     <h5 class="mb-0">
                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -18,29 +18,29 @@ function Search_Box() {
                         </button>
                     </h5>
                 </div>
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion1">
                     <div class="card-body">
                         <User_Buttons />
                     </div>
                 </div>
             </div>
-            <div class="card">
+            <div class="card" id="accordion2">
                 <div class="card-header" id="headingTwo">
                     <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                             <div className="row">
                                 <h3>New Search</h3>
                             </div>
                         </button>
                     </h5>
                 </div>
-                <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion2">
                     <div class="card-body">
                         < User_Input />
                     </div>
                 </div>
             </div>
-            <div class="card d-lg-none">
+            <div class="card d-lg-none" id="accordion3">
                 <div class="card-header" id="headingThree">
                     <h5 class="mb-0">
                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
@@ -50,14 +50,13 @@ function Search_Box() {
                         </button>
                     </h5>
                 </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion3">
                     <div class="card-body">
 
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 

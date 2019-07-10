4428ff6eb2289cfd2aea4c6a7bcca0fb36507b17
HermesCat <jppettis@comcast.net>
2019-07-09 20:26:28 -0400

    my commit

--- client/src/components/User_Input/index.js
+++ client/src/components/User_Input/index.js
@@ -3,38 +3,38 @@ import "./style.css";
 
 
 
-function User_Input () {
+function User_Input() {
 
 
 
-    return (
-      <>
+  return (
+    <>
 
-          <div className="row">
-            <div class="input-group mb-3">
-              <div className="input-group-prepend">
-                <span
-                  className="input-group-text"
-                  id="inputGroup-sizing-default"
-                >
-                  Keyword
+      <div className="row">
+        <div class="input-group mb-3">
+          <div className="input-group-prepend">
+            <span
+              className="input-group-text"
+              id="inputGroup-sizing-default"
+            >
+              Keyword
                 </span>
-              </div>
-              <input
-                type="text"
-                className="form-control"
-                aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default"
-              />
-            </div>
           </div>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </div>
+      </div>
 
-         
-        
-        
-      </>
-    );
-  }
+
+
+
+    </>
+  );
+}
 
 
 export default User_Input;

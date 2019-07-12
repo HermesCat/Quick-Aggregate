b7f1ae2b739b462887c251421f47d8c4b6628ca2
Ty Wood <twood06@gmail.com>
2019-07-11 20:29:59 -0400

    added search controller/route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const usersRoutes = require("./users");
+const searchRoutes = require("./search")
 
 // user routes
 router.use("/users", usersRoutes);
+
+// search routes
+router.use("/search", searchRoutes)
 
 module.exports = router;

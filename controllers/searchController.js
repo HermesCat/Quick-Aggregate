baa01e1960cb60a84c7180b0adab345064e01c80
Ty Wood <twood06@gmail.com>
2019-07-13 10:27:34 -0400

    controllers and routes added

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,9 +2,23 @@ const db = require('../models');
 
 module.exports = {
 	create: function(req, res) {
-        db.Search
-        .find(req.query)
-        .then((dbModel) => res.json(dbModel))
-        .catch((err) => res.status(422).json(err));
-	}
+                db.Search
+                .create(req.query)
+                .then((dbModel) => res.json(dbModel))
+                .catch((err) => res.status(422).json(err));
+	},
+
+	findAll: function(req, res) {
+                db.Search
+                .find(req.query)
+                .then(dbModel => res.json(dbModel))
+                .catch(err => res.status(422).json(err));
+        },
+
+        findById: function(req, res) {
+                db.Search
+                .findById(req.params.id)
+                .then(dbModel => res.json(dbModel))
+                .catch(err => res.status(422).json(err));
+        }
 };
--- routes/api/search.js
+++ routes/api/search.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const searchController = require("../../controllers/searchController");
 
+//search route
 router.route("/")
-    .post(searchController.create);
-
+    .post(searchController.create)
+    .get(searchController.findAll)
+    
+//search route ID
+router.route("/:id")
+    .get(searchController.findById)
 
 module.exports = router;

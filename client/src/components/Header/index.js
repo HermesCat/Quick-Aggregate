d71d938f89e53dc74c44ec5185220e9d1ab47419
HermesCat <jppettis@comcast.net>
2019-07-18 19:16:53 -0400

    working on login modal

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,34 +10,56 @@ import HamburgerIcon from "./img/hamburger_icon.png";
 
 function Header() {
     return (
-
-        <header>
-            <div className="row">
-                <div className="col-6">
-                    <h1 className="main-title"><span className="highligher">Quick</span>Aggregate</h1>
-                </div>
-                <div className="col-3" />
-                <div className="col-3 navigation">
-                    <a className="d-lg-none "><img className="hamburger-icon" src={HamburgerIcon} /></a>
-                    <Router>
-                        <>
-                            <Nav />
-                            <div className="row  button-list">
-                                <div className="col-12">
-                                    <button id="login">Login</button>
-                                    <a href="/register"><button id="sign-up">Sign-Up</button></a>
+        <>
+            <header>
+                <div className="row">
+                    <div className="col-6">
+                        <h1 className="main-title"><span className="highligher">Quick</span>Aggregate</h1>
+                    </div>
+                    <div className="col-3" />
+                    <div className="col-3 navigation">
+                        <a className="d-lg-none "><img className="hamburger-icon" src={HamburgerIcon} /></a>
+                        <Router>
+                            <>
+                                <Nav />
+                                <div className="row  button-list">
+                                    <div className="col-12">
+                                        <button type="button" class="btn" id="login" data-toggle="modal" data-target="#loginModal">
+                                            Login
+                                        </button>
+                                        <a href="/register"><button id="sign-up">Sign-Up</button></a>
+                                    </div>
                                 </div>
-                            </div>
-                            <Switch>
-                                <Route exact path="/" />
-                                <Route exact path="/" />
-                                <Route exact path="/" />
-                            </Switch>
-                        </>
-                    </Router>
+                                <Switch>
+                                    <Route exact path="/" />
+                                    <Route exact path="/" />
+                                    <Route exact path="/" />
+                                </Switch>
+                            </>
+                        </Router>
+                    </div>
+                </div>
+            </header>
+            <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 className="modal-image-stripe">About Quick Aggregate</h4>>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+    </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </header>
+        </>
     );
 }
 

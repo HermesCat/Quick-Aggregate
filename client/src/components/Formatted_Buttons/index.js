708134455c6042014cb0367434d56ef4bc835130
unknown <chugm1989@gmail.com>
2019-07-20 10:37:16 -0400

    media button colors

--- client/src/components/Formatted_Buttons/index.js
+++ client/src/components/Formatted_Buttons/index.js
@@ -6,7 +6,7 @@ function Formatted_Buttons(props) {
 
     return (
         <div>
-            <button className="user-keyword"
+            <button className={`user-keyword ${props.api}-buttons`}
             id={props.search}
             onClick={() => props.saveBtnSearch(props.api, props.search)}>
             {props.search} 
